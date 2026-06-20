cfa40da2a9227a538b28696d395262e39af902fa
nagarjunak02 <nagarjuna472@gmail.com>
2026-06-20 08:51:09 +0530

    Started with objects and curruntly learning about static and non static memebers

---
+++ src/Core/Object/bike/BikeMain.java
@@ -0,0 +1,13 @@
+package Core.Object.bike;
+
+public class BikeMain {
+    public static void main(String[] args) {
+        Bike b= new Bike();
+        b.cc = 300;
+        b.color = "Blue";
+        b.brand = "Tvs Apache RTX";
+
+        b.stopBike();
+        b.stopBike();
+    }
+}
